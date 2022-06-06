0e3c644f3d3d304b3a59f925beb1a34504829d2d
parsher <26663229+parsher@users.noreply.github.com>
2022-06-06 13:05:36 +0900

    feature: init, view controller, naviagtion

---
+++ ScreenTransitionExample/SeguePushViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SuguePushViewController.swift
+//  ScreenTransitionExample
+//
+
+import UIKit
+
+class SeguePushViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func tapBackButton(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+        
+        //self.navigationController?.popToRootViewController(animated: true)
+    }
+}
---
+++ ScreenTransitionExample/SuguePushController.swift
@@ -0,0 +1,12 @@
+//
+//  SuguePushController.swift
+//  ScreenTransitionExample
+//
+//  Created by mmj on 2022/06/06.
+//
+
+import UIKit
+
+class SuguePushController: NSObject {
+
+}
